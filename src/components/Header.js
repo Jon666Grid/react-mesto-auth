8d9a467fd70ba3523aaea863d119b20d66a2e5d5
Jon666Grid <jon666gridin@yandex.ru>
2022-08-09 20:13:51 +0200

    Проектная работа :12 Место

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,23 @@
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState } from 'react';
 import logo from "../images/Logo.svg";
 import { Link, Route } from 'react-router-dom';
 
 export default function Header(props) {
-   const [isClicked, setIsClicked] = useState(false);
-   const [iswindow, setIsWindow] = useState(0);
-
-   //*Проверка ширины окна просмотра. 
-   const resizeWindow = useCallback(() => {
-      setIsWindow(window.innerWidth);
-      if (iswindow >= 639) {
-         setIsClicked(true);
-      }
-   }, [iswindow]);
+   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
    //* Проверка ширины окна при монтировании компонентов.
    useEffect(() => {
-      resizeWindow();
+      const resizeWindow = (() => {
+         if (window.innerWidth >= 639) {
+            setIsMenuOpen(true)
+         }
+      });
       window.addEventListener("resize", resizeWindow);
       return () => window.removeEventListener("resize", resizeWindow);
-   }, [resizeWindow]);
+   }, []);
 
    function handleClickMenu() {
-      setIsClicked(!isClicked)
+      setIsMenuOpen(!isMenuOpen)
    }
 
    return (
@@ -45,12 +40,12 @@ export default function Header(props) {
          <Route exact path="/">
             <div className='header__burger-menu'>
                <button type="button"
-                  className={`${isClicked ? 'header__button header__button_close' : 'header__button header__button_burger'}`}
+                  className={`${isMenuOpen ? 'header__button header__button_close' : 'header__button header__button_burger'}`}
                   onClick={handleClickMenu}>
                   <span></span>
                </button>
             </div>
-            <nav className={`${isClicked ? 'header__user-menu' : 'not-active'}`}>
+            <nav className={`${isMenuOpen ? 'header__user-menu' : 'not-active'}`}>
                <p className='header__email'>{props.email}</p>
                <button
                   onClick={() => {
@@ -60,7 +55,6 @@ export default function Header(props) {
                   className='header__link header__link_button'>Выйти</button>
             </nav>
          </Route>
-
       </header>
    );
 };
